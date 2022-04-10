e7bfdf4695ce9a09d48502d19846c07d005f3553
Yanxiang Deng <dyxmvp@users.noreply.github.com>
2022-04-10 09:36:14 -0400

    Add files via upload

---
+++ Data_visualization/SpatialPlot_traj.R
@@ -0,0 +1,440 @@
+SpatialPlot_traj <- function (object, group.by = NULL, features = NULL, images = NULL, 
+          cols = NULL, image.alpha = 1, crop = TRUE, slot = "data", 
+          min.cutoff = NA, max.cutoff = NA, cells.highlight = NULL, 
+          cols.highlight = c("#DE2D26", "grey50"), facet.highlight = FALSE, 
+          label = FALSE, label.size = 5, label.color = "white", 
+          label.box = TRUE, repel = FALSE, ncol = NULL, combine = TRUE, 
+          pt.size.factor = 1.6, alpha = c(1, 1), stroke = 0.25, interactive = FALSE, 
+          do.identify = FALSE, identify.ident = NULL, do.hover = FALSE, 
+          information = NULL) 
+{
+  if (isTRUE(x = do.hover) || isTRUE(x = do.identify)) {
+    warning("'do.hover' and 'do.identify' are deprecated as we are removing plotly-based interactive graphics, use 'interactive' instead for Shiny-based interactivity", 
+            call. = FALSE, immediate. = TRUE)
+    interactive <- TRUE
+  }
+  if (!is.null(x = group.by) & !is.null(x = features)) {
+    stop("Please specific either group.by or features, not both.")
+  }
+  images <- images %||% Images(object = object, assay = DefaultAssay(object = object))
+  if (is.null(x = features)) {
+    if (interactive) {
+      return(ISpatialDimPlot(object = object, image = image, 
+                             group.by = group.by, alpha = alpha))
+    }
+    group.by <- group.by %||% "ident"
+    object[["ident"]] <- Idents(object = object)
+    data <- object[[group.by]]
+    for (group in group.by) {
+      if (!is.factor(x = data[, group])) {
+        data[, group] <- factor(x = data[, group])
+      }
+    }
+  }
+  else {
+    if (interactive) {
+      return(ISpatialFeaturePlot(object = object, feature = features[1], 
+                                 image = images[1], slot = slot, alpha = alpha))
+    }
+    data <- FetchData(object = object, vars = features, slot = slot)
+    features <- colnames(x = data)
+    min.cutoff <- mapply(FUN = function(cutoff, feature) {
+      return(ifelse(test = is.na(x = cutoff), yes = min(data[, 
+                                                             feature]), no = cutoff))
+    }, cutoff = min.cutoff, feature = features)
+    max.cutoff <- mapply(FUN = function(cutoff, feature) {
+      return(ifelse(test = is.na(x = cutoff), yes = max(data[, 
+                                                             feature]), no = cutoff))
+    }, cutoff = max.cutoff, feature = features)
+    check.lengths <- unique(x = vapply(X = list(features, 
+                                                min.cutoff, max.cutoff), FUN = length, FUN.VALUE = numeric(length = 1)))
+    if (length(x = check.lengths) != 1) {
+      stop("There must be the same number of minimum and maximum cuttoffs as there are features")
+    }
+    data <- sapply(X = 1:ncol(x = data), FUN = function(index) {
+      data.feature <- as.vector(x = data[, index])
+      min.use <- SetQuantile(cutoff = min.cutoff[index], 
+                             data.feature)
+      max.use <- SetQuantile(cutoff = max.cutoff[index], 
+                             data.feature)
+      data.feature[data.feature < min.use] <- min.use
+      data.feature[data.feature > max.use] <- max.use
+      return(data.feature)
+    })
+    colnames(x = data) <- features
+    rownames(x = data) <- Cells(x = object)
+  }
+  if (length(x = images) == 0) {
+    images <- Images(object = object)
+  }
+  if (length(x = images) < 1) {
+    stop("Could not find any spatial image information")
+  }
+  features <- colnames(x = data)
+  colnames(x = data) <- features
+  rownames(x = data) <- colnames(x = object)
+  facet.highlight <- facet.highlight && (!is.null(x = cells.highlight) && 
+                                           is.list(x = cells.highlight))
+  if (do.hover) {
+    if (length(x = images) > 1) {
+      images <- images[1]
+      warning("'do.hover' requires only one image, using image ", 
+              images, call. = FALSE, immediate. = TRUE)
+    }
+    if (length(x = features) > 1) {
+      features <- features[1]
+      type <- ifelse(test = is.null(x = group.by), yes = "feature", 
+                     no = "grouping")
+      warning("'do.hover' requires only one ", type, 
+              ", using ", features, call. = FALSE, immediate. = TRUE)
+    }
+    if (facet.highlight) {
+      warning("'do.hover' requires no faceting highlighted cells", 
+              call. = FALSE, immediate. = TRUE)
+      facet.highlight <- FALSE
+    }
+  }
+  if (facet.highlight) {
+    if (length(x = images) > 1) {
+      images <- images[1]
+      warning("Faceting the highlight only works with a single image, using image ", 
+              images, call. = FALSE, immediate. = TRUE)
+    }
+    ncols <- length(x = cells.highlight)
+  }
+  else {
+    ncols <- length(x = images)
+  }
+  plots <- vector(mode = "list", length = length(x = features) * 
+                    ncols)
+  for (i in 1:ncols) {
+    plot.idx <- i
+    image.idx <- ifelse(test = facet.highlight, yes = 1, 
+                        no = i)
+    image.use <- object[[images[[image.idx]]]]
+    coordinates <- GetTissueCoordinates(object = image.use)
+    highlight.use <- if (facet.highlight) {
+      cells.highlight[i]
+    }
+    else {
+      cells.highlight
+    }
+    for (j in 1:length(x = features)) {
+      cols.unset <- is.factor(x = data[, features[j]]) && 
+        is.null(x = cols)
+      if (cols.unset) {
+        cols <- hue_pal()(n = length(x = levels(x = data[, 
+                                                         features[j]])))
+        names(x = cols) <- levels(x = data[, features[j]])
+      }
+      plot <- SingleSpatialPlot(data = cbind(coordinates, 
+                                             data[rownames(x = coordinates), features[j], 
+                                                  drop = FALSE]), image = image.use, image.alpha = image.alpha, 
+                                col.by = features[j], cols = cols, alpha.by = if (is.null(x = group.by)) {
+                                  features[j]
+                                }
+                                else {
+                                  NULL
+                                }, geom = if (inherits(x = image.use, what = "STARmap")) {
+                                  "poly"
+                                }
+                                else {
+                                  "spatial"
+                                }, cells.highlight = highlight.use, cols.highlight = cols.highlight, 
+                                pt.size.factor = pt.size.factor, stroke = stroke, 
+                                crop = crop)
+      if (is.null(x = group.by)) {
+        # plot <- plot + scale_fill_gradientn(name = features[j], 
+        #                                     colours = SpatialColors(n = 100)) + theme(legend.position = "top") + 
+        #   scale_alpha(range = alpha) + guides(alpha = FALSE)
+        
+        plot <- plot + scale_fill_gradientn(name = features[j], 
+                                            colours = ArchRPalettes$horizonExtra, oob = scales::squish) + theme(legend.position = "top") + 
+          scale_alpha(range = alpha) + guides(alpha = FALSE)
+      }
+      else if (label) {
+        plot <- LabelClusters(plot = plot, id = ifelse(test = is.null(x = cells.highlight), 
+                                                       yes = features[j], no = "highlight"), 
+                              geom = if (inherits(x = image.use, what = "STARmap")) {
+                                "GeomPolygon"
+                              }
+                              else {
+                                "GeomSpatial"
+                              }, repel = repel, size = label.size, color = label.color, 
+                              box = label.box, position = "nearest")
+      }
+      if (j == 1 && length(x = images) > 1 && !facet.highlight) {
+        plot <- plot + ggtitle(label = images[[image.idx]]) + 
+          theme(plot.title = element_text(hjust = 0.5))
+      }
+      if (facet.highlight) {
+        plot <- plot + ggtitle(label = names(x = cells.highlight)[i]) + 
+          theme(plot.title = element_text(hjust = 0.5)) + 
+          NoLegend()
+      }
+      plots[[plot.idx]] <- plot
+      plot.idx <- plot.idx + ncols
+      if (cols.unset) {
+        cols <- NULL
+      }
+    }
+  }
+  if (length(x = images) > 1 && combine) {
+    plots <- wrap_plots(plots = plots, ncol = length(x = images))
+  }
+  else if (length(x = images == 1) && combine) {
+    plots <- wrap_plots(plots = plots, ncol = ncol)
+  }
+  return(plots)
+}
+
+
+###
+SetQuantile <- function(cutoff, data) {
+  if (grepl(pattern = '^q[0-9]{1,2}$', x = as.character(x = cutoff), perl = TRUE)) {
+    this.quantile <- as.numeric(x = sub(
+      pattern = 'q',
+      replacement = '',
+      x = as.character(x = cutoff)
+    )) / 100
+    data <- unlist(x = data)
+    data <- data[data > 0]
+    cutoff <- quantile(x = data, probs = this.quantile)
+  }
+  return(as.numeric(x = cutoff))
+}
+
+
+###
+SingleSpatialPlot <- function(
+  data,
+  image,
+  cols = NULL,
+  image.alpha = 1,
+  crop = TRUE,
+  pt.size.factor = NULL,
+  stroke = 0.25,
+  col.by = NULL,
+  alpha.by = NULL,
+  cells.highlight = NULL,
+  cols.highlight = c('#DE2D26', 'grey50'),
+  geom = c('spatial', 'interactive', 'poly'),
+  na.value = 'grey50'
+) {
+  geom <- match.arg(arg = geom)
+  if (!is.null(x = col.by) && !col.by %in% colnames(x = data)) {
+    warning("Cannot find '", col.by, "' in data, not coloring", call. = FALSE, immediate. = TRUE)
+    col.by <- NULL
+  }
+  col.by <- col.by %iff% paste0("`", col.by, "`")
+  alpha.by <- alpha.by %iff% paste0("`", alpha.by, "`")
+  if (!is.null(x = cells.highlight)) {
+    highlight.info <- SetHighlight(
+      cells.highlight = cells.highlight,
+      cells.all = rownames(x = data),
+      sizes.highlight = pt.size.factor,
+      cols.highlight = cols.highlight[1],
+      col.base = cols.highlight[2]
+    )
+    order <- highlight.info$plot.order
+    data$highlight <- highlight.info$highlight
+    col.by <- 'highlight'
+    levels(x = data$ident) <- c(order, setdiff(x = levels(x = data$ident), y = order))
+    data <- data[order(data$ident), ]
+  }
+  plot <- ggplot(data = data, aes_string(
+    x = colnames(x = data)[2],
+    y = colnames(x = data)[1],
+    fill = col.by,
+    alpha = alpha.by
+  ))
+  plot <- switch(
+    EXPR = geom,
+    'spatial' = {
+      plot + geom_spatial(
+        point.size.factor = pt.size.factor,
+        data = data,
+        image = image,
+        image.alpha = image.alpha,
+        crop = crop,
+        stroke = stroke
+      ) + coord_fixed()
+    },
+    'interactive' = {
+      plot + geom_spatial_interactive(
+        data = tibble(grob = list(GetImage(object = image, mode = 'grob'))),
+        mapping = aes_string(grob = 'grob'),
+        x = 0.5,
+        y = 0.5
+      ) +
+        geom_point(mapping = aes_string(color = col.by)) +
+        xlim(0, ncol(x = image)) +
+        ylim(nrow(x = image), 0) +
+        coord_cartesian(expand = FALSE)
+    },
+    'poly' = {
+      data$cell <- rownames(x = data)
+      data[, c('x', 'y')] <- NULL
+      data <- merge(
+        x = data,
+        y = GetTissueCoordinates(object = image, qhulls = TRUE),
+        by = "cell"
+      )
+      plot + geom_polygon(
+        data = data,
+        mapping = aes_string(fill = col.by, group = 'cell')
+      ) + coord_fixed() + theme_cowplot()
+      
+    },
+    stop("Unknown geom, choose from 'spatial' or 'interactive'", call. = FALSE)
+  )
+  if (!is.null(x = cells.highlight)) {
+    plot <- plot + scale_fill_manual(values = cols.highlight)
+  }
+  if (!is.null(x = cols) && is.null(x = cells.highlight)) {
+    if (length(x = cols) == 1 && (is.numeric(x = cols) || cols %in% rownames(x = brewer.pal.info))) {
+      scale <- scale_fill_brewer(palette = cols, na.value = na.value)
+    } else if (length(x = cols) == 1 && (cols %in% c('alphabet', 'alphabet2', 'glasbey', 'polychrome', 'stepped'))) {
+      colors <- DiscretePalette(length(unique(data[[col.by]])), palette = cols)
+      scale <- scale_fill_manual(values = colors, na.value = na.value)
+    } else {
+      scale <- scale_fill_manual(values = cols, na.value = na.value)
+    }
+    plot <- plot + scale
+  }
+  plot <- plot + theme_void()
+  return(plot)
+}
+
+
+###
+`%||%` <- function(lhs, rhs) {
+  if (!is.null(x = lhs)) {
+    return(lhs)
+  } else {
+    return(rhs)
+  }
+}
+
+
+###
+`%iff%` <- function(lhs, rhs) {
+  if (!is.null(x = lhs)) {
+    return(rhs)
+  } else {
+    return(lhs)
+  }
+}
+
+
+###
+geom_spatial <-  function(
+  mapping = NULL,
+  data = NULL,
+  image = image,
+  image.alpha = image.alpha,
+  crop = crop,
+  stat = "identity",
+  position = "identity",
+  na.rm = FALSE,
+  show.legend = NA,
+  inherit.aes = TRUE,
+  ...
+) {
+  layer(
+    geom = GeomSpatial,
+    mapping = mapping,
+    data = data,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, image = image, image.alpha = image.alpha, crop = crop, ...)
+  )
+}
+
+
+###
+GeomSpatial <- ggproto(
+  "GeomSpatial",
+  Geom,
+  required_aes = c("x", "y"),
+  extra_params = c("na.rm", "image", "image.alpha", "crop"),
+  default_aes = aes(
+    shape = 21,
+    colour = "black",
+    point.size.factor = 1.0,
+    fill = NA,
+    alpha = NA,
+    stroke = 0.25
+  ),
+  setup_data = function(self, data, params) {
+    data <- ggproto_parent(Geom, self)$setup_data(data, params)
+    # We need to flip the image as the Y coordinates are reversed
+    data$y = max(data$y) - data$y + min(data$y)
+    data
+  },
+  draw_key = draw_key_point,
+  draw_panel = function(data, panel_scales, coord, image, image.alpha, crop) {
+    # This should be in native units, where
+    # Locations and sizes are relative to the x- and yscales for the current viewport.
+    if (!crop) {
+      y.transform <- c(0, nrow(x = image)) - panel_scales$y.range
+      data$y <- data$y + sum(y.transform)
+      panel_scales$x$continuous_range <- c(0, nrow(x = image))
+      panel_scales$y$continuous_range <- c(0, ncol(x = image))
+      panel_scales$y.range <- c(0, nrow(x = image))
+      panel_scales$x.range <- c(0, ncol(x = image))
+    }
+    z <- coord$transform(
+      data.frame(x = c(0, ncol(x = image)), y = c(0, nrow(x = image))),
+      panel_scales
+    )
+    # Flip Y axis for image
+    z$y <- -rev(z$y) + 1
+    wdth <- z$x[2] - z$x[1]
+    hgth <- z$y[2] - z$y[1]
+    vp <- viewport(
+      x = unit(x = z$x[1], units = "npc"),
+      y = unit(x = z$y[1], units = "npc"),
+      width = unit(x = wdth, units = "npc"),
+      height = unit(x = hgth, units = "npc"),
+      just = c("left", "bottom")
+    )
+    img.grob <- GetImage(object = image)
+    
+    img <- editGrob(grob = img.grob, vp = vp)
+    # spot.size <- slot(object = image, name = "spot.radius")
+    spot.size <- Radius(object = image)
+    coords <- coord$transform(data, panel_scales)
+    pts <- pointsGrob(
+      x = coords$x,
+      y = coords$y,
+      pch = data$shape,
+      size = unit(spot.size, "npc") * data$point.size.factor,
+      gp = gpar(
+        col = alpha(colour = coords$colour, alpha = coords$alpha),
+        fill = alpha(colour = coords$fill, alpha = coords$alpha),
+        lwd = coords$stroke)
+    )
+    vp <- viewport()
+    gt <- gTree(vp = vp)
+    if (image.alpha > 0) {
+      if (image.alpha != 1) {
+        img$raster = as.raster(
+          x = matrix(
+            data = alpha(colour = img$raster, alpha = image.alpha),
+            nrow = nrow(x = img$raster),
+            ncol = ncol(x = img$raster),
+            byrow = TRUE)
+        )
+      }
+      gt <- addGrob(gTree = gt, child = img)
+    }
+    gt <- addGrob(gTree = gt, child = pts)
+    # Replacement for ggname
+    gt$name <- grobName(grob = gt, prefix = 'geom_spatial')
+    return(gt)
+    # ggplot2:::ggname("geom_spatial", gt)
+  }
+)
